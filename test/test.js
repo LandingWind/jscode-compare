9e7b33a409fd7a0ac7dd912ca2848094f0631428
wkk5194 <kysoowjk@163.com>
2020-06-29 16:34:39 +0800

    modify

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const ast = require('jscode-compare').default;
 
 (async () => {
-    const cmpRes = await ast.compare('./demo.js', './demo2.js', true)
-    console.log(cmpRes)
+    // const cmpRes = await ast.compare('./demo.js', './demo2.js', true)
+    // console.log(cmpRes)
+    await ast.parseCodeToAST('./demo.js', './demo-ast.js');
 })()
